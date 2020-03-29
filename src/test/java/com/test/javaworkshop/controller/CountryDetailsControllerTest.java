5a7bc83ddb26d76a7349eba676c210e3dd231e54
Elgebally <islam-mostafa.elgebally@dxc.com>
2020-03-29 22:54:03 +0200

    adding docker compose

--- src/test/java/com/test/javaworkshop/controller/CountryDetailsControllerTest.java
+++ src/test/java/com/test/javaworkshop/controller/CountryDetailsControllerTest.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.test.javaworkshop.exception.CountryNotFoundException;
 import com.test.javaworkshop.exception.DataBaseNotAvailableException;
 import com.test.javaworkshop.service.impl.CountryDetailsService;
 
@@ -35,15 +34,18 @@ class CountryDetailsControllerTest {
 
 	@Test
 	void testgetCountryNonCodeExist() throws Exception {
-		String worngCode = "40";
+
+		String worngCode = "wrngcode";
+		Mockito.when(countryDetailsService.findByCountryCode(worngCode)).thenThrow(new CountryNotFoundException(ErrorResponseEnum.INVALID_COUNTRY_CODE.name()));
 		String URI = "/" + worngCode;
 		mockMvc.perform(MockMvcRequestBuilders.get(URI).accept(MediaType.APPLICATION_JSON)).andExpect(status().is5xxServerError()).andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ErrorResponseEnum.INVALID_COUNTRY_CODE.name()));
 	}
+
 	@Test
 	void testgetCountryDatabaseDown() throws Exception {
 
+		String Code = "wrngcode";
 		Mockito.when(countryDetailsService.findByCountryCode(Mockito.anyString())).thenThrow(new DataBaseNotAvailableException(ErrorResponseEnum.INTERNAL_ERROR.name()));
-		String Code = "10";
 		String URI = "/" + Code;
 		mockMvc.perform(MockMvcRequestBuilders.get(URI).accept(MediaType.APPLICATION_JSON)).andExpect(status().is5xxServerError()).andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ErrorResponseEnum.INTERNAL_ERROR.name()));
 	}
